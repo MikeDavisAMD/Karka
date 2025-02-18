c53e995518cb0a2a253573d110cfc316784ba884
MikeDavisAMD <mikedavisantony@gmail.com>
2025-02-18 10:47:59 +0530

    18th Feb 2025

--- DOM/DOM JSON/JS/todolist.js
+++ DOM/DOM JSON/JS/todolist.js
@@ -1,14 +1,14 @@
 function clickHandle(){
     let d=document.getElementById("div")
-    let input=document.getElementById("input").value
+    let inp=document.getElementById("input").value
     let list=document.createElement("li")
-    btn=`<button onclick="edit()">✏️</button>
-    <button onclick="del(parentNode)">🗑️</button>`
-    list.innerHTML=input+" "+btn
+    input=`${inp} <button onclick="edit()">EDIT</button>
+    <button onclick="del(parentNode)">DELETE</button>`
+    list.innerHTML=input
     d.appendChild(list)
     document.getElementById("input").value=""
 }
-function edit(list){
+function edit(){
     
 }
 function del(a){
